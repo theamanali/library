a70514f8dddc471c987cc6cc0f09eec7a334085e
Aman Ali <amanali@uw.edu>
2025-05-20 00:05:43 -0700

    Create DisplayController and FormController classes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
-const noBooksText = document.querySelector('.no-books-text');
-const libraryContainer = document.querySelector('.library-container');
 const mainAddButton = document.querySelector('.addBookMain');
-let mainDeleteButtons = document.querySelectorAll('#delete-button');
-const addBookDialog = document.querySelector('#add-book-dialog');
 const dialogCancelButton = document.querySelector('.cancelDialog');
+const addBookDialog = document.querySelector('#add-book-dialog');
 const dialogSubmitButton = document.querySelector('.addBookFormButton');
-const form = document.querySelector('form');
 
 class Library {
     libraryArray;
@@ -27,10 +23,6 @@ class Library {
             if (this.libraryArray[i].id === id) {
                 this.libraryArray.splice(i, 1);
             }
-        }
-
-        if (this.libraryArray.length === 0) {
-            showNoBooksText();
         }
     }
 
@@ -55,93 +47,124 @@ class Book {
     }
 }
 
-const myLibrary = new Library();
+class DisplayController {
+    constructor() {
+        this.noBooksText = document.querySelector('.no-books-text');
+        this.libraryContainer = document.querySelector('.library-container');
+    }
 
-function hideNoBooksText() {
-    noBooksText.hidden = true;
-}
+    hideNoBooksText() {
+        this.noBooksText.hidden = true;
+    }
 
-function showNoBooksText() {
-    noBooksText.hidden = false;
-}
+    showNoBooksText() {
+        this.noBooksText.hidden = false;
+    }
 
-function deleteBook(id) {
-    const bookCards = document.querySelectorAll('.book-card');
-    for (let i = 0; i < bookCards.length; i++) {
-        if (bookCards[i].dataset.id === id) {
-            libraryContainer.removeChild(bookCards[i]);
-            return;
+    deleteBook(id) {
+        const bookCards = document.querySelectorAll('.book-card');
+
+        if (bookCards.length === 1) {
+            this.showNoBooksText()
+        }
+
+        for (let i = 0; i < bookCards.length; i++) {
+            if (bookCards[i].dataset.id === id) {
+                this.libraryContainer.removeChild(bookCards[i]);
+                return;
+            }
         }
     }
+
+    displayNewBook(book) {
+        if (library.libraryArray.length === 0) {
+            return;
+        }
+
+        this.hideNoBooksText();
+
+        const newBookCard = document.createElement('div');
+        const newBookContent = document.createElement('div');
+        const title = document.createElement('h3');
+        const author = document.createElement('p');
+        const buttonContainer = document.createElement('div');
+        const readLabel = document.createElement('label');
+        const readButton = document.createElement('input');
+        const deleteButton = document.createElement('button');
+
+        // add values and styles to elements
+        newBookCard.classList.add('book-card');
+        newBookCard.setAttribute('data-id', book.id);
+        newBookContent.classList.add('book-content');
+        title.textContent = book.title;
+        author.textContent = book.author;
+        buttonContainer.classList.add('button-container');
+        readLabel.setAttribute('for', 'read');
+        readButton.setAttribute('type', 'checkbox');
+        readButton.setAttribute("name", "read")
+        readButton.setAttribute("id", "read");
+        if (book.hasBeenRead) {
+            readButton.checked = true;
+        }
+        deleteButton.setAttribute("type", "button");
+        deleteButton.setAttribute("id", "delete-button");
+        deleteButton.textContent = "Delete";
+
+        // put elements in order
+        readLabel.appendChild(readButton);
+        newBookContent.appendChild(title);
+        newBookContent.appendChild(author);
+        buttonContainer.appendChild(readLabel);
+        buttonContainer.appendChild(deleteButton);
+        newBookCard.appendChild(newBookContent);
+        newBookCard.appendChild(buttonContainer);
+        this.libraryContainer.prepend(newBookCard);
+    }
 }
 
-function displayNewBook(book) {
-    if (myLibrary.libraryArray.length === 0) {
-        return;
+class FormController {
+    constructor() {
+        this.form = document.querySelector('form');
     }
 
-    hideNoBooksText();
-    
-    const newBookCard = document.createElement('div');
-    const newBookContent = document.createElement('div');
-    const title = document.createElement('h3');
-    const author = document.createElement('p');
-    const buttonContainer = document.createElement('div');
-    const readLabel = document.createElement('label');
-    const readButton = document.createElement('input');
-    const deleteButton = document.createElement('button');
-
-    // add values and styles to elements
-    newBookCard.classList.add('book-card');
-    newBookCard.setAttribute('data-id', book.id);
-    newBookContent.classList.add('book-content');
-    title.textContent = book.title;
-    author.textContent = book.author;
-    buttonContainer.classList.add('button-container');
-    readLabel.setAttribute('for', 'read');
-    readButton.setAttribute('type', 'checkbox');
-    readButton.setAttribute("name", "read")
-    readButton.setAttribute("id", "read");
-    if (book.hasBeenRead) {
-        readButton.checked = true;
+    get formAuthor() {
+        return this.form.elements['author'].value;
     }
-    deleteButton.setAttribute("type", "button");
-    deleteButton.setAttribute("id", "delete-button");
-    deleteButton.textContent = "Delete";
 
-    // put elements in order
-    readLabel.appendChild(readButton);
-    newBookContent.appendChild(title);
-    newBookContent.appendChild(author);
-    buttonContainer.appendChild(readLabel);
-    buttonContainer.appendChild(deleteButton);
-    newBookCard.appendChild(newBookContent);
-    newBookCard.appendChild(buttonContainer);
-    libraryContainer.prepend(newBookCard);
-    mainDeleteButtons = document.querySelectorAll('#delete-button');
+    get formTitle() {
+        return this.form.elements['title'].value;
+    }
+
+    get formHasBeenRead() {
+        return this.form.elements['hasBeenRead'].checked;
+    }
+
+    clearForm() {
+        this.form.elements['title'].value = "";
+        this.form.elements['author'].value = "";
+        this.form.elements['hasBeenRead'].checked = false;
+    }
+
+    static hasValidFormInput(title, author) {
+        return title.trim().length > 0 && author.trim().length > 0;
+    }
 }
 
-function hasValidFormInput(title, author) {
-    return title.trim().length > 0 && author.trim().length > 0;
-}
-
-function clearForm() {
-    form.elements['title'].value = "";
-    form.elements['author'].value = "";
-    form.elements['hasBeenRead'].checked = false;
-}
+const library = new Library();
+const displayController = new DisplayController();
+const formController = new FormController();
 
 document.body.addEventListener('click', (e) => {
     let bookId;
     
     if (e.target.matches('#delete-button')) {
         bookId = e.target.parentElement.parentElement.dataset.id;
-        deleteBook(bookId);
-        myLibrary.removeBookFromLibrary(bookId);
+        displayController.deleteBook(bookId);
+        library.removeBookFromLibrary(bookId);
     }
     else if (e.target.matches('#read')) {
         bookId = e.target.parentElement.parentElement.parentElement.dataset.id;
-        myLibrary.markRead(bookId);
+        library.markRead(bookId);
     }
 });
 
@@ -155,14 +178,14 @@ dialogCancelButton.addEventListener('click', () => {
 
 dialogSubmitButton.addEventListener('click', (e) => {
     e.preventDefault()
-    const title = form.elements['title'].value;
-    const author = form.elements['author'].value;
-    const hasBeenRead = form.elements['hasBeenRead'].checked;
+    const title = formController.formTitle;
+    const author = formController.formAuthor;
+    const hasBeenRead = formController.formHasBeenRead;
 
-    if(hasValidFormInput(title, author)) {
-        myLibrary.addBookToLibrary(title, author, hasBeenRead);
-        displayNewBook(myLibrary.libraryArray[myLibrary.libraryArray.length - 1]);
-        clearForm()
+    if(FormController.hasValidFormInput(title, author)) {
+        library.addBookToLibrary(title, author, hasBeenRead);
+        displayController.displayNewBook(library.libraryArray[library.libraryArray.length - 1]);
+        formController.clearForm()
         addBookDialog.close();
     }
     else {
